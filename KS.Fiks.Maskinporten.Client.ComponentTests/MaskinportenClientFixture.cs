474c7d956a2afd0f02969a295f9357632627b869
Sveinung Fjær <sveinung.fjaer@webstep.no>
2019-02-26 13:53:22 +0100

    Added integration tests

--- KS.Fiks.Maskinporten.Client.ComponentTests/MaskinportenClientFixture.cs
+++ KS.Fiks.Maskinporten.Client.ComponentTests/MaskinportenClientFixture.cs
@@ -1,19 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using Ks.Fiks.Maskinporten.Client;
 
 namespace KS.Fiks.Maskinporten.Client.ComponentTests
 {
     public class MaskinportenClientFixture
     {
-        private MaskinportenClientProperties _properties;
+        private readonly MaskinportenClientProperties _properties;
+
+        private X509Certificate2 _certificate;
+
+        private const string MaskinportenValidateEndpoint =
+            "https://oidc-ver2.difi.no/idporten-oidc-provider/tokeninfo";
+
+        private const string IdPortenCertFile = ""; // Insert path to certificate to run locally
+
+        private const string IdPortenCertPass = ""; // Insert credentials to run locally
+
+        private const string MaskinportenTokenEndpoint = @"https://oidc-ver2.difi.no/idporten-oidc-provider/token";
+        private const string MaskinportenAudience = @"https://oidc-ver2.difi.no/idporten-oidc-provider/";
+        private const string MaskinportenIssuer = @"oidc_ks_test";
+        private const int MaskinportenNumberOfSecondsLeftBeforeExpire = 10;
 
         public MaskinportenClientFixture()
         {
-            _properties = new MaskinportenClientProperties("test", "http://fikst.test.ks.no", "test", 1);
+            if (CanRunTestWithProperCredentials())
+            {
+                _certificate = new X509Certificate2(IdPortenCertFile, IdPortenCertPass);
+            }
+            else
+            {
+                _certificate = new X509Certificate2("alice-virksomhetssertifikat.p12",
+                    "PASSWORD");
+            }
+
+            _properties = new MaskinportenClientProperties(MaskinportenAudience, MaskinportenTokenEndpoint,
+                MaskinportenIssuer, MaskinportenNumberOfSecondsLeftBeforeExpire);
         }
-        
+
         public MaskinportenClient CreateSut()
         {
-            return new MaskinportenClient(_properties);
+            return new MaskinportenClient(_certificate, _properties);
         }
+
+        public MaskinportenClientFixture WithUnauthorizedCertificate()
+        {
+            _certificate = new X509Certificate2("alice-virksomhetssertifikat.p12",
+                "PASSWORD");
+            return this;
+        }
+
+        public MaskinportenClientFixture WithAHighNumberOfSecondsLeftBeforeExpire()
+        {
+            _properties.NumberOfSecondsLeftBeforeExpire = 1000000;
+            return this;
+        }
+
+        public async Task<HttpResponseMessage> ValidateTokenWithIdPorten(MaskinportenToken token)
+        {
+            var httpClient = new HttpClient();
+            var content = new FormUrlEncodedContent(new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("token", token.JwtId)
+            });
+
+            return await httpClient.PostAsync(MaskinportenValidateEndpoint, content);
+        }
+
+        // This is needed until a proper way to inject credentials on the server is found
+        public bool CanRunTestWithProperCredentials()
+        {
+            return IdPortenCertFile.Length > 0;
+        }
+        
     }
 }
+
--- KS.Fiks.Maskinporten.Client.ComponentTests/MaskinportenClientTests.cs
+++ KS.Fiks.Maskinporten.Client.ComponentTests/MaskinportenClientTests.cs
@@ -1,11 +1,17 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using FluentAssertions;
+using Ks.Fiks.Maskinporten.Client;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace KS.Fiks.Maskinporten.Client.ComponentTests
 {
     public class MaskinportenClientComponentTests
     {
-        private MaskinportenClientFixture _fixture;
+        private readonly MaskinportenClientFixture _fixture;
 
 
         public MaskinportenClientComponentTests()
@@ -13,11 +19,78 @@ namespace KS.Fiks.Maskinporten.Client.ComponentTests
             _fixture = new MaskinportenClientFixture();
         }
 
+        
         [Fact]
         public void CanBeCreated()
         {
             var sut = _fixture.CreateSut();
             sut.Should().NotBe(null);
+        }
+
+        [Fact]
+        public async Task GetsUnexpectedResponseIfCertificateIsWrong()
+        {
+            await Assert.ThrowsAsync<UnexpectedResponseException>(async () =>
+            {
+                var sut = _fixture.WithUnauthorizedCertificate().CreateSut();
+                var token = await sut.GetAccessToken("ks");
+            });
+        }
+        
+        [Fact]
+        public async Task GetsTokenIfCertificationIsValid()
+        {
+            if (!_fixture.CanRunTestWithProperCredentials())
+            {
+                return;
+            }
+            
+            var sut = _fixture.CreateSut();
+            var token = await sut.GetAccessToken("ks");
+            token.Should().BeOfType<MaskinportenToken>();
+        }
+        
+        [Fact]
+        public async Task GetsTheSameTokenIfCallingTwiceInShortTime()
+        {
+            if (!_fixture.CanRunTestWithProperCredentials())
+            {
+                return;
+            }
+            
+            var sut = _fixture.CreateSut();
+            var token1 = await sut.GetAccessToken("ks");
+            var token2 = await sut.GetAccessToken("ks");
+            token1.Should().Be(token2);
+        }
+        
+        [Fact]
+        public async Task GetsNewTokenIfNumberOfSecondsLeftBeforeExpireIsLargerThanExp()
+        {
+            if (!_fixture.CanRunTestWithProperCredentials())
+            {
+                return;
+            }
+            
+            var sut = _fixture.WithAHighNumberOfSecondsLeftBeforeExpire().CreateSut();
+            var token1 = await sut.GetAccessToken("ks");
+            
+            var token2 = await sut.GetAccessToken("ks");
+            token1.Should().NotBe(token2);
+        }
+
+        [Fact]
+        public async Task TokenIsValidatedAtIdPorten()
+        {
+            if (!_fixture.CanRunTestWithProperCredentials())
+            {
+                return;
+            }
+            
+            var sut = _fixture.CreateSut();
+            var token = await sut.GetAccessToken("ks");
+            var response = await _fixture.ValidateTokenWithIdPorten(token);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
         }
     }
 }
